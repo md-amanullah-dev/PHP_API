f954b2cf4d5d64ee7e1767d4875f9611758a1a4a
AMANULLAH023HY <amana170829@gmail.com>
2025-09-29 11:47:16 +0530

    PHP project setup and base structutre and welcome api

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+// Errors visible (development ke liye)
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// JSON header
+header("Content-Type: application/json");
+
+// Simple welcome response
+echo json_encode([
+    "message" => "Welcome to API"
+]);
